cc77e4ba2bdca5476f5e962ff8ae0f819d48f4ad
angelicamartinez34 <angelicammaria34@gmail.com>
2023-10-25 20:31:42 -0600

    week07assignment

--- eclipse-workspace/mysql-java/src/main/java/projects/dao/DbConnection.java
+++ eclipse-workspace/mysql-java/src/main/java/projects/dao/DbConnection.java
@@ -4,16 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import recipes.exception.DbException;
+import projects.exception.DbException;
 
 public class DbConnection {
 
+	
 	private static String HOST = "localhost";
 	private static String PASSWORD = "projects";
 	private static int PORT = 3306;
 	private static String SCHEMA = "projects";
 	private static String USER = "projects";
 	
+	/**
+	 * @return
+	 */
 	public static Connection getConnection() {
 		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER, PASSWORD);
 		
@@ -30,3 +34,5 @@ System.out.println(" Connecting with url" + uri);
 		}		
 	}
 }
+
+
--- eclipse-workspace/mysql-java/src/main/java/projects/exception/DbException.java
+++ eclipse-workspace/mysql-java/src/main/java/projects/exception/DbException.java
@@ -1,8 +1,9 @@
 package projects.exception;
 
-@SuppressWarnings("serial")
 public class DbException extends RuntimeException {
 
+
+	private static final long serialVersionUID = 1L;
 	public DbException(String message) {}
 	public DbException(Throwable cause) {}
 	public DbException(String message, Throwable cause) {}
